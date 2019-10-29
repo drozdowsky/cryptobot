f15275025a63612323bec960b72dadcb5d849fb9
Jan Mróz <janmroz97@gmail.com>
2019-10-29 13:46:07 +0100

    Change simple database sql

--- docs/db-schema-simple.sql
+++ docs/db-schema-simple.sql
@@ -1,21 +1,3 @@
-CREATE TABLE public.auth_group (
-    id integer NOT NULL,
-    name character varying(150) NOT NULL
-);
-
-CREATE TABLE public.auth_group_permissions (
-    id integer NOT NULL,
-    group_id integer NOT NULL,
-    permission_id integer NOT NULL
-);
-
-CREATE TABLE public.auth_permission (
-    id integer NOT NULL,
-    name character varying(255) NOT NULL,
-    content_type_id integer NOT NULL,
-    codename character varying(100) NOT NULL
-);
-
 CREATE TABLE public.auth_user (
     id integer NOT NULL,
     password character varying(128) NOT NULL,
@@ -28,18 +10,7 @@ CREATE TABLE public.auth_user (
     is_staff boolean NOT NULL,
     is_active boolean NOT NULL,
     date_joined timestamp with time zone NOT NULL
-);
-
-CREATE TABLE public.auth_user_groups (
-    id integer NOT NULL,
-    user_id integer NOT NULL,
-    group_id integer NOT NULL
-);
-
-CREATE TABLE public.auth_user_user_permissions (
-    id integer NOT NULL,
-    user_id integer NOT NULL,
-    permission_id integer NOT NULL
+	amount numeric(19,2) NOT NULL
 );
 
 CREATE TABLE public.crypto_cryptomodel (
@@ -54,11 +25,6 @@ CREATE TABLE public.crypto_cryptowallet (
     crypto_id integer NOT NULL,
     owner_id integer NOT NULL,
     date timestamp with time zone NOT NULL
-);
-
-CREATE TABLE public.crypto_currencywallet (
-    owner_id integer NOT NULL,
-    amount numeric(19,2) NOT NULL
 );
 
 CREATE TABLE public.crypto_markethistoric (
@@ -102,35 +68,3 @@ CREATE TABLE public.crypto_trade (
     price numeric(19,2) NOT NULL,
     rule_set_id integer
 );
-
-CREATE TABLE public.django_admin_log (
-    id integer NOT NULL,
-    action_time timestamp with time zone NOT NULL,
-    object_id text,
-    object_repr character varying(200) NOT NULL,
-    action_flag smallint NOT NULL,
-    change_message text NOT NULL,
-    content_type_id integer,
-    user_id integer NOT NULL,
-    CONSTRAINT django_admin_log_action_flag_check CHECK ((action_flag >= 0))
-);
-
-CREATE TABLE public.django_content_type (
-    id integer NOT NULL,
-    app_label character varying(100) NOT NULL,
-    model character varying(100) NOT NULL
-);
-
-CREATE TABLE public.django_migrations (
-    id integer NOT NULL,
-    app character varying(255) NOT NULL,
-    name character varying(255) NOT NULL,
-    applied timestamp with time zone NOT NULL
-);
-
-CREATE TABLE public.django_session (
-    session_key character varying(40) NOT NULL,
-    session_data text NOT NULL,
-    expire_date timestamp with time zone NOT NULL
-);
-
